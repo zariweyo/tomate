d72f315120fde0e949aec26cb3275c6170e52e30
Daniel Cañete <danielcanetecampos1978@gmail.com>
2026-06-13 10:47:36 +0200

    Use sprite for F1 car and cap off-track speed

--- game/src/entities/Car.js
+++ game/src/entities/Car.js
@@ -5,14 +5,9 @@ export class Car {
     this.speed = 0;
     this.rotationDegrees = startPosition.angle;
 
-    this.body = scene.add.container(startPosition.x, startPosition.y);
-    this.sprite = scene.add.rectangle(0, 0, carData.width, carData.height, 0xdd2222);
-    this.frontWing = scene.add.rectangle(0, -carData.height / 2 + 4, carData.width + 10, 5, 0xffffff);
-    this.rearWing = scene.add.rectangle(0, carData.height / 2 - 4, carData.width + 8, 5, 0x111111);
-    this.cockpit = scene.add.rectangle(0, -4, carData.width * 0.55, carData.height * 0.32, 0x111111);
-
-    this.body.add([this.sprite, this.frontWing, this.rearWing, this.cockpit]);
-    this.body.setRotation(Phaser.Math.DegToRad(this.rotationDegrees));
+    this.sprite = scene.add.image(startPosition.x, startPosition.y, carData.spriteKey);
+    this.sprite.setDisplaySize(carData.width, carData.height);
+    this.sprite.setRotation(Phaser.Math.DegToRad(this.rotationDegrees));
   }
 
   update(deltaSeconds, inputState, isOnTrack) {
@@ -21,8 +16,10 @@ export class Car {
     }
 
     const friction = isOnTrack ? this.carData.friction : this.carData.offTrackFriction;
+    const currentMaxSpeed = isOnTrack ? this.carData.maxSpeed : this.carData.offTrackMaxSpeed;
+
     this.speed *= friction;
-    this.speed = Phaser.Math.Clamp(this.speed, 0, this.carData.maxSpeed);
+    this.speed = Phaser.Math.Clamp(this.speed, 0, currentMaxSpeed);
 
     if (this.speed > 8) {
       if (inputState.turningLeft) {
@@ -35,15 +32,15 @@ export class Car {
     }
 
     const rotationRadians = Phaser.Math.DegToRad(this.rotationDegrees - 90);
-    this.body.x += Math.cos(rotationRadians) * this.speed * deltaSeconds;
-    this.body.y += Math.sin(rotationRadians) * this.speed * deltaSeconds;
-    this.body.setRotation(Phaser.Math.DegToRad(this.rotationDegrees));
+    this.sprite.x += Math.cos(rotationRadians) * this.speed * deltaSeconds;
+    this.sprite.y += Math.sin(rotationRadians) * this.speed * deltaSeconds;
+    this.sprite.setRotation(Phaser.Math.DegToRad(this.rotationDegrees));
   }
 
   getPosition() {
     return {
-      x: this.body.x,
-      y: this.body.y
+      x: this.sprite.x,
+      y: this.sprite.y
     };
   }
 }
